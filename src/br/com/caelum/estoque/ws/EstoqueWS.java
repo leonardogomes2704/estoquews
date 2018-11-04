333edc4c93b0eb6bbded35aa00bd0f9c826f64bf
Leonardo Gomes <leonardogomes2704@gmail.com>
2018-11-04 16:30:55 -0200

    Usando e Validando token

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -1,45 +1,23 @@
 package br.com.caelum.estoque.ws;
 
+
+
 import java.util.List;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
 import javax.jws.WebService;
 
-import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
-import br.com.caelum.estoque.modelo.item.ListaItens;
-import br.com.caelum.estoque.modelo.usuario.TokenDao;
-import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
-import br.com.caelum.estoque.usuario.AutorizacaoException;
 
 @WebService
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
 	
-	@WebMethod(operationName="TodosOsItens")
-	@WebResult(name="itens")
-	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
+	
+	public List<Item> getItens(){
 		List<Item> lista = dao.todosItens();
-		return new ListaItens(lista);
-	}
-	
-	@WebMethod(operationName="CadastrarItem")
-	@WebResult(name="item")
-	public Item cadastrarItem(@WebParam(name="tokenUsuario", header=true) TokenUsuario token, @WebParam(name="item") Item item) throws AutorizacaoException {
-		System.out.println("Cadastrando Item "+ item + "Token " + token);
-		
-		boolean valido = new TokenDao().ehValido(token);
-		if(!valido) {
-			throw new AutorizacaoException("Autorização Falhou.");
-		}
-			this.dao.cadastrar(item);
-	
-		
-		return item;
+		return lista;
 	}
 
 }
--- src/br/com/caelum/estoque/ws/PublicaWebService.java
+++ src/br/com/caelum/estoque/ws/PublicaWebService.java
@@ -8,8 +8,6 @@ public class PublicaWebService {
 		EstoqueWS service =  new EstoqueWS();
 		String url = "http://localhost:8080/estoquews";
 		
-		System.out.println("Serviço rodando " + url + "?wsdl" );
-		
 		Endpoint.publish(url, service);
 
 	}
